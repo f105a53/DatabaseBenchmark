0ef7195074950246ca2c6f01619633cabad1b9ba
J2ghz <j2.00ghz@gmail.com>
2019-05-27 15:43:43 +0200

    works

--- DatabaseBenchmark/SelectJoinBenchmark.cs
+++ DatabaseBenchmark/SelectJoinBenchmark.cs
@@ -1,8 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using BenchmarkDotNet.Attributes;
 using DatabaseBenchmark.Model.EFCore;
 using DatabaseBenchmark.Model.Linq2Db;
+using LinqToDB;
 using LinqToDB.Data;
 using LinqToDB.DataProvider.SqlServer;
 
@@ -12,13 +14,14 @@ namespace DatabaseBenchmark
     [MarkdownExporter]
     [RPlotExporter]
     [MemoryDiagnoser]
-    [ReturnValueValidator(false)]
+    [ReturnValueValidator(true)]
     public class SelectJoinBenchmark
     {
         public enum Workload
         {
             SelectEmployees,
-            SelectEmployeeCount
+            SelectEmployeeCount,
+            UpdateEmployee
         }
 
         private readonly CompanyContext EfCoreContext = new CompanyContext();
@@ -34,23 +37,42 @@ namespace DatabaseBenchmark
 
         [ParamsAllValues] public Workload SelectedWorkload { get; set; }
 
-        [Benchmark]
+        [Benchmark(Baseline = true)]
         public ICollection<int> EFCore()
         {
+            int UpdateEmployee()
+            {
+                var employee = EfCoreContext.Employee.Find(123456789m);
+                employee.Address = DateTimeOffset.Now.ToString();
+                EfCoreContext.Update(employee);
+                EfCoreContext.SaveChanges();
+                return 1;
+            }
+
             return SelectedWorkload switch
                 {
-                Workload.SelectEmployees => EfCoreContext.Employee.ToListSignleItem().Count.ToListSignleItem(),
-                Workload.SelectEmployeeCount => EfCoreContext.Employee.Count().ToListSignleItem()
+                Workload.SelectEmployees => EfCoreContext.Employee.ToList().Count.ToListSignleItem(),
+                Workload.SelectEmployeeCount => EfCoreContext.Employee.Count().ToListSignleItem(),
+                Workload.UpdateEmployee => UpdateEmployee().ToListSignleItem(),
                 };
         }
 
         [Benchmark]
         public ICollection<int> Linq2Db()
         {
+            int UpdateEmployee()
+            {
+                var employee = Linq2DbContext.Employees.Find(123456789);
+                employee.Address = DateTimeOffset.Now.ToString();
+                Linq2DbContext.Update(employee);
+                return 1;
+            }
+
             return SelectedWorkload switch
                 {
                 Workload.SelectEmployees => Linq2DbContext.Employees.ToList().Count.ToListSignleItem(),
-                Workload.SelectEmployeeCount => Linq2DbContext.Employees.Count().ToListSignleItem()
+                Workload.SelectEmployeeCount => Linq2DbContext.Employees.Count().ToListSignleItem(),
+                Workload.UpdateEmployee => UpdateEmployee().ToListSignleItem(),
                 };
         }
     }
